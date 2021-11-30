80320b7beea092fa5629cb307fa69e3f4cd373e9
n-rdy <giuseppegelardi@icloud.com>
2021-11-30 10:43:30 -0600

    add route to add new3 new user

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,4 @@
-const { User } = require('../models/user')
+const { User, validateUser } = require('../models/user')
 const { Product, validate } = require('../models/product')
 const express = require('express')
 const router = express.Router()
@@ -85,5 +85,26 @@ router.delete('./:userId/shoppingcart/:productId', async (req, res) => {
 // start jwt tutorial
 
 // add new user
+router.post('/', async (req, res) => {
+  try {
+    const { error } = validateUser(req.body)
+
+    if (error) return res.status(400).send(error.details[0].message)
+
+    let user = await User.findOne({ email: req.body.email })
+    if (user) return res.status(400).send('User already registered.')
+
+    user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    })
+
+    await user.save()
+    return res.send({ _id: user._id, name: user.name, email: user.email })
+  } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`)
+  }
+}) 
 
 module.exports = router
