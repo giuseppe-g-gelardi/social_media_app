f54ebac3e6d1fc3380263019a2facb0099b901d2
n-rdy <giuseppegelardi@icloud.com>
2021-11-29 20:38:58 -0600

    add routes and models from previous project

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const express = require('express')
 const cors = require('cors')
 const connectDB = require('./connect/mongoConnect')
 
+const comments = require('./routes/comment')
+
 const app = express()
 
 connectDB()
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/comments', comments)
 
 const port = process.env.PORT || 5000
 
 app.listen(port, () => {
-  console.log(`Starting server on port: ${port}`)
+  console.log(`Server started on port: ${port}`)
 })
