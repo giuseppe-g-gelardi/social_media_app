eaafefe49699e7d0f13729b5fc7670fbdb5d5418
n-rdy <giuseppegelardi@icloud.com>
2021-11-30 11:26:54 -0600

    add product models

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const connectDB = require('./connect/mongoConnect')
 
 const comments = require('./routes/comment')
+const users = require('./routes/users')
 
 const app = express()
 
@@ -11,6 +12,7 @@ connectDB()
 app.use(cors())
 app.use(express.json())
 app.use('/api/comments', comments)
+app.use('/api/users', users)
 
 const port = process.env.PORT || 5000
 
