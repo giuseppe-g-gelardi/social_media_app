05e870df15a37df0514457411a267f2fdbee566a
n-rdy <giuseppegelardi@icloud.com>
2021-11-30 20:04:44 -0600

    import dotenv to auth middleware, set proper jwt

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,15 @@
 const jwt = require('jsonwebtoken')
-const config = require('config')
+// const config = require('config')
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 function auth (req, res, next) {
   const token = req.header('x-auth-token')
-  
+
   if (!token) return res.status(401).send('Access denied. No token provided.')
   try {
-    const decoded = jwt.verify(token, config.get('jwtSecret'))
+    const decoded = jwt.verify(token, process.env.JWT)
     req.user = decoded
     return next()
   } catch (ex) {
