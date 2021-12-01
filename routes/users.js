3e0eb5ed2347322823ddd904b26955193f32b74c
n-rdy <giuseppegelardi@icloud.com>
2021-11-30 21:04:46 -0600

    add endpoint to query all users

--- routes/users.js
+++ routes/users.js
@@ -129,4 +129,15 @@ router.post('/', async (req, res) => {
   }
 }) 
 
+
+// get ALL reqest
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find()
+    return res.send(users)
+  } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`)
+  }
+})
+
 module.exports = router
