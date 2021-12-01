6ad0725f215a72368c697e79f219e7d3409c9709
RandallHammond23 <hrandall31@gmail.com>
2021-11-30 19:00:52 -0600

    add login validation. add jsonwebtoken.

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,30 @@
 const Joi = require('joi');
 const express = require('express');
 const router = express.Router();
+const bcrypt = require ('bcrypt');
+const {User} = require('../models/user');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+
+
+router.post("/", async (req, res) => {
+  try {
+    const { error } = validateLogin(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send("Invalid email or password.");
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword)
+      return res.status(400).send("Invalid email or password.");
+    return res.send(true);
+  } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+  }
+});
+
 
 function validateLogin(req) {
   const schema = Joi.object({
