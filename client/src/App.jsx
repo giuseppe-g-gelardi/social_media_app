c8e711b207f1fdf174ad8664787c954ac906f791
n-rdy <giuseppegelardi@icloud.com>
2021-12-01 20:49:10 -0600

    start basic register form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
+// import { Routes, Route } from 'react-router-dom'
 import { ThemeProvider } from '@material-ui/core'
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import { lightTheme, darkTheme } from './components/theme/Theme'
 import MainAppbar from './components/MainAppbar';
-import Home from './pages/Home'
-import Landing from './pages/Landing'
-import Error from './pages/Error'
+// import Home from './pages/Home'
+// import Landing from './pages/Landing'
+// import Error from './pages/Error'
+import RegistrationPage from './pages/RegistrationPage';
 
 
 export default function App() {
@@ -19,11 +20,13 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
         <MainAppbar check={darkMode} change={() => setDarkMode(!darkMode)} />
-          <Routes>
+          <RegistrationPage />
+
+          {/* <Routes>
             <Route path='/' element={<Landing />} />
             <Route path='/home' element={<Home />} />
             <Route path='*' element={<Error />} />
-          </Routes>
+          </Routes> */}
     </ThemeProvider>
   )
 }
