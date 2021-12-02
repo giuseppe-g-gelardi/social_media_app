94ce0fd5c7fe8dc4356eb9585336a04f9d66a0d8
n-rdy <giuseppegelardi@icloud.com>
2021-12-02 13:45:07 -0600

    start context and global state to track if a user is logged in or not

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react'
-// import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { ThemeProvider } from '@material-ui/core'
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import { lightTheme, darkTheme } from './components/theme/Theme'
 import MainAppbar from './components/MainAppbar';
-// import Home from './pages/Home'
-// import Landing from './pages/Landing'
-// import Error from './pages/Error'
+import Home from './pages/Home'
+import Landing from './pages/Landing'
+import Error from './pages/Error'
 import RegistrationPage from './pages/RegistrationPage';
 import LoginPage from './pages/LoginPage';
+
+import UserState from './context/UserState'
 
 
 export default function App() {
@@ -23,12 +25,16 @@ export default function App() {
         <MainAppbar check={darkMode} change={() => setDarkMode(!darkMode)} />
           <RegistrationPage />
           <LoginPage />
+          
+<UserState>
 
-          {/* <Routes>
+          <Routes>
             <Route path='/' element={<Landing />} />
             <Route path='/home' element={<Home />} />
             <Route path='*' element={<Error />} />
-          </Routes> */}
+          </Routes>
+</UserState>
+
     </ThemeProvider>
   )
 }
---
+++ client/src/context/UserState.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import UserContext from './userContext'
+
+export default function UserState() {
+  return (
+    <UserContext.Provider value={{
+      // isAuth,
+      // setIsAuth
+    }}>
+      
+    </UserContext.Provider>
+  )
+}
