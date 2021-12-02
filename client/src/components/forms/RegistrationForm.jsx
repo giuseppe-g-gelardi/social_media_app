4a0ec9e7c44125ac9d095745c1130355403302e3
n-rdy <giuseppegelardi@icloud.com>
2021-12-02 09:56:23 -0600

    register new user working correctly

--- client/src/components/forms/RegistrationForm.jsx
+++ client/src/components/forms/RegistrationForm.jsx
@@ -8,89 +8,85 @@ const useStyles = makeStyles({
   field: {
     marginTop: 10,
     marginBottom: 10,
-    display: 'block',
+    display: 'block'
   },
   text: {
     marginTop: 10,
     marginBottom: 10,
     display: 'block',
-    color: '#bd93f9',
-  },
+    color: '#bd93f9'
+  }
 })
 
-export default function RegistrationForm() {
-
-  const [ userName, setUserName ] = useState('')
-  const [ userEmail, setUserEmail ] = useState('')
-  const [ userPassword, setUserPassword ] = useState('')
-  const [ newUser, setNewUser ] = useState({})
+export default function RegistrationForm () {
+  const [userName, setUserName] = useState('')
+  const [userEmail, setUserEmail] = useState('')
+  const [userPassword, setUserPassword] = useState('')
   const classes = useStyles()
-
   const api = `http://localhost:8000/api/users/`
 
   const handleSubmit = async e => {
     e.preventDefault()
-    setNewUser({
-      "name": userName,
-      "email": userEmail,
-      "password": userPassword
-    })
-  
-    try { 
-      await axios.post(api, newUser).then(response => {
+
+    const user = {
+      name: userName,
+      email: userEmail,
+      password: userPassword
+    }
+    axios
+      .post(api, user)
+      .then(response => {
         console.log(response)
       })
-    } catch (error) {
-      console.log(error)
-    }
+      .catch(error => {
+        console.log(`Axios error: `, error)
+      })
   }
 
   return (
     <Container>
+      <form onSubmit={handleSubmit}>
+        <FormControl>
+          <TextField
+            style={{ marginBottom: 20 }}
+            onChange={e => setUserName(e.target.value)}
+            className={classes.field}
+            label='Enter your name'
+            variant='outlined'
+            fullWidth
+            required
+          />
 
-      <FormControl onSubmit={handleSubmit}>
+          <TextField
+            style={{ marginBottom: 20 }}
+            onChange={e => setUserEmail(e.target.value)}
+            className={classes.field}
+            label='Enter your email'
+            variant='outlined'
+            fullWidth
+            required
+          />
 
-        <TextField 
-          style={{marginBottom: 20}}
-          onChange={e => setUserName(e.target.value)}
-          className={classes.field}
-          label='Enter your name'
-          variant='outlined'
-          fullWidth
-          required
-        />
+          <TextField
+            style={{ marginBottom: 20 }}
+            onChange={e => setUserPassword(e.target.value)}
+            className={classes.field}
+            label='Enter a password'
+            variant='outlined'
+            fullWidth
+            required
+          />
 
-        <TextField 
-          style={{marginBottom: 20}}
-          onChange={e => setUserEmail(e.target.value)}
-          className={classes.field}
-          label='Enter your email'
-          variant='outlined'
-          fullWidth
-          required
-        />
-
-        <TextField 
-          style={{marginBottom: 20}}
-          onChange={e => setUserPassword(e.target.value)}
-          className={classes.field}
-          label='Enter a password'
-          variant='outlined'
-          fullWidth
-          required
-        />
-
-        <Button
-          type='submit'
-          color='primary'       
-          variant='contained'
-          endIcon={<KeyboardArrowRightIcon />}
-        >
-          Register Account!
-        </Button>
-
-      </FormControl>
-      
+          <Button
+            type='submit'
+            color='primary'
+            variant='contained'
+            endIcon={<KeyboardArrowRightIcon />}
+          >
+            Register Account!
+          </Button>
+        </FormControl>
+      </form>
     </Container>
   )
 }
