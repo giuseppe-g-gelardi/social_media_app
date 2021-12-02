06e254615cd215cd3fd6fb4460f388c5f90b6273
Sebastian Maxwell <sebastianrmaxwell@gmail.com>
2021-12-01 22:11:09 -0500

    Create private message model and route. Needs testing on postman

--- models/privatemessages.js
+++ models/privatemessages.js
@@ -1,23 +1,51 @@
-const Joi = require("joi");
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const dateAndTime = require("date-and-time");
+const date = new Date();
 
-
-const messageSchema = new mongoose.Schema({
-    userID:{ type:String, required:true },
-    text: { type: String, required: true },
+const messageShema = Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    default: dateAndTime.format(date, "hh:mm:ss A, MM-DD-YYYY"),
+  },
 });
 
-function validateMessage(message) {
-    const schema = Joi.object({
-      userID: Joi.string().min(1).max(255).required(),
-      text: Joi.string().min(1).max(255).required(),
-      timestamp: { type: Date, default: Date.now()}
+module.exports = mongoose.model("message", messageShema);
+
+
+
+
+
+
+
+// const Joi = require("joi");
+// const mongoose = require("mongoose");
+
+
+// const messageSchema = new mongoose.Schema({
+//     userID:{ type:String, required:true },
+//     text: { type: String, required: true },
+// });
+
+// function validateMessage(message) {
+//     const schema = Joi.object({
+//       userID: Joi.string().min(1).max(255).required(),
+//       text: Joi.string().min(1).max(255).required(),
+//       timestamp: { type: Date, default: Date.now()}
       
-    });
-    return schema.validate(message);
-  }
+//     });
+//     return schema.validate(message);
+//   }
 
-const Message = mongoose.model("Message", messageSchema);
+// const Message = mongoose.model("Message", messageSchema);
 
-module.exports.Message = Message;
-module.exports.validateMessage = validateMessage;
+// module.exports.Message = Message;
+// module.exports.validateMessage = validateMessage;
--- routes/privatemessages.js
+++ routes/privatemessages.js
@@ -1,3 +1,64 @@
+const router = express.Router();
+const { check, validationResult } = require("express-validator");
+
+//Private Message Model//
+
+const Privatemessages = require("../models/privatemessages");
+
+//User Model//
+
+const User = require("../models/user");
+
+Router.post(
+    "/:username/message",
+    [check("text", "Please Provide Message").not().isEmpty()],
+    async(req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ error: errors.array() });
+    }
+
+    const { text } = req.body;
+
+    try {
+      const user = await User.findOne({ username: req.params.username }).select(
+        "-password"
+      );
+
+      if (!user) {
+        res.json({ error: [{ msg: "User not found" }] });
+      }
+
+      let message = new Message({
+        user: user.id,
+        text,
+      });
+
+      message = await message.save();
+
+      res.status(200).json(message);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ error: [{ msg: "Server Error" }] });
+    }
+  }
+);
+
+module.exports = Router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // const express = require('express')
 // const router = express.Router()
 // const auth = require('../middleware/auth')
@@ -17,3 +78,5 @@
 // })
 
 // module.exports = router
+
+
