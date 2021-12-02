d5ee6a538ed027f358b8356ee7a1dd58b385926f
n-rdy <giuseppegelardi@icloud.com>
2021-12-01 21:10:27 -0600

    add basic login page

--- client/src/components/forms/LoginForm.jsx
+++ client/src/components/forms/LoginForm.jsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import { FormControl, Container, Button, TextField } from '@material-ui/core'
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+  field: {
+    marginTop: 10,
+    marginBottom: 10,
+    display: 'block',
+  },
+  text: {
+    marginTop: 10,
+    marginBottom: 10,
+    display: 'block',
+    color: '#bd93f9',
+  },
+})
 
 export default function LoginForm() {
+
+  const [ userEmail, setUserEmail ] = useState('')
+  const [ userPassword, setUserPassword ] = useState('')
+  const [ loginUser, setLoginUser ] = useState({})
+  const classes = useStyles()
+
+  const api = `http://localhost:8000/api/auth`
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    setLoginUser({
+      "email": userEmail,
+      "password": userPassword
+    })
+  
+    try { 
+      await axios.post(api, loginUser).then(response => {
+        console.log(response)
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <div>
+    <Container>
+
+      <FormControl onSubmit={handleSubmit}>
+
+        <TextField 
+          style={{marginBottom: 20}}
+          onChange={e => setUserEmail(e.target.value)}
+          className={classes.field}
+          label='Enter your email'
+          variant='outlined'
+          fullWidth
+          required
+        />
+
+        <TextField 
+          style={{marginBottom: 20}}
+          onChange={e => setUserPassword(e.target.value)}
+          className={classes.field}
+          label='Enter a password'
+          variant='outlined'
+          fullWidth
+          required
+        />
+
+        <Button
+          type='submit'
+          color='primary'       
+          variant='contained'
+          endIcon={<KeyboardArrowRightIcon />}
+        >
+          Login
+        </Button>
+
+      </FormControl>
       
-    </div>
+    </Container>
   )
 }
--- client/src/pages/RegistrationPage.jsx
+++ client/src/pages/RegistrationPage.jsx
@@ -4,7 +4,7 @@ import { Button, Container } from '@material-ui/core'
 import RegistrationForm from '../components/forms/RegistrationForm'
 import Controls from '../components/controls/Controls'
 
-export default function RegistrationPage(props) {
+export default function RegistrationPage() {
 
   const [openPopup, setOpenPopup] = useState(false)
 
