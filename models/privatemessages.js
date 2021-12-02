da0213afd74132ebe80dfae1f3f6be92538ae620
RandallHammond23 <hrandall31@gmail.com>
2021-12-01 19:44:36 -0600

    add private message model

---
+++ models/privatemessages.js
@@ -0,0 +1,22 @@
+const Joi = require("joi");
+const mongoose = require("mongoose");
+
+
+const messageSchema = new mongoose.Schema({
+    userID:{ type:String, required:true },
+    text: { type: String, required: true },
+});
+
+function validateMessage(message) {
+    const schema = Joi.object({
+      userID: Joi.string().min(1).max(255).required(),
+      text: Joi.string().min(1).max(255).required(),
+      
+    });
+    return schema.validate(message);
+  }
+
+const Message = mongoose.model("Message", messageSchema);
+
+module.exports.Message = Message;
+module.exports.validateMessage = validateMessage;
