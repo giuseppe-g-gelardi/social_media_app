9dce9c5abb7c80899c0af9f15898671f666342f9
n-rdy <giuseppegelardi@icloud.com>
2021-12-03 11:40:02 -0600

    remove context from frontend

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,10 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 import './index.css'
-// import { AuthProvider } from './context/AuthContext'
 
 ReactDOM.render(
-  // <AuthProvider>
     <Router>
       <App />
-    </Router>
-  //</AuthProvider>
-  ,
+    </Router>,
   document.getElementById('root')
 )
