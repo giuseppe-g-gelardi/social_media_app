7b520c906108dec1bb823ba21d43de22a5841070
RandallHammond23 <hrandall31@gmail.com>
2021-12-03 19:11:49 -0600

    add friend request route. add change friend route.

--- routes/friend.js
+++ routes/friend.js
@@ -1,43 +1,35 @@
-const { Friends } = require("../models/friends");
+const { Friends, validateFriends } = require("../models/friends");
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user");
 
 
-// get all friends
-router.get("/user/friends", async (req, res) => {
-    try {
-      const friend = await Friends.find();
-      return res.send(friend);
-    } catch (ex) {
-      return res.status(500).send(`Internal Server Error: ${ex}`);
-    }
+
+
+
+
+router.get("/userID/Friends", async (req, res) => {
+  try {
+    const friend = await Friends.find();
+    return res.send(friend);
+  } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+  }
 });
 
 
 
-// get a single friend
-router.get("/user/friends/:id", async (req, res) => {
-    try {
-      const friend = await Friends.findById(req.params.id);
-      if (!friend)
-        return res.status(400).send(`The reply with  "${req.params.id}" does not exist.`);
-      return res.send(friend);
-    } catch (ex) {
-      return res.status(500).send(`Internal Server Error: ${ex}`);
-    }
+router.get("/:userID/Friends/:id", async (req, res) => {
+  try {
+    const friend = await Friends.findById(req.params.id);
+    if (!friend)
+      return res.status(400).send(`The friend with id "${req.params.id}" does not exist.`);
+    return res.send(friend);
+  } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+  }
 });
 
 
 
-// delete friend
-router.delete("/user/friends/:id", async (req, res) => {
-    try {
-      const friend = await Friends.findByIdAndRemove(req.params.id);
-      if (!friend)
-        return res.status(400).send(` "${req.params.id}" does not exist.`);
-      return res.send(friend);
-    } catch (ex) {
-      return res.status(500).send(`Internal Server Error: ${ex}`);
-    }
-});
-
+module.exports = router
---
+++ routes/friendRequest.js
@@ -0,0 +1,132 @@
+const {FriendRequest, validateFriendRequest} = require("../models/friendRequests");
+const express = require("express");
+const router = express.Router();
+
+
+
+
+
+
+router.post("/api/:userID/sendReq", auth, async (req, res) => {
+    try {
+      const temp = await Friends.findById({
+        requestedTo: req.params.userID,
+        requestedBy: req.params.userID,
+      });
+  
+      if (!temp) {
+        const friend = new friend({
+          requestedTo: req.params.userID,
+          requestedBy: req.params.userID,
+          status: 0,
+        });
+        await friend.save();
+      } else {
+        await Friends.deleteOne({
+          requestedTo: req.params.userID,
+          requestedBy: req.params.userID,
+        });
+        const user1 = await user1.findById({ userID: req.params.userID });
+        user1.follower = user1.follower - 1;
+        await user1.save();
+        const user2 = await user1.findById({ userID: req.params.userID });
+        user2.following = user2.following - 1;
+        await user2.save();
+      }
+  
+      res.send();
+    } catch (e) {
+      res.status(500).send(e);
+    }
+  });
+
+
+
+  
+router.get("/api/req", auth, async (req, res) => {
+    try {
+      const requests = await Friends.find({
+        requestedTo: req.body.userID,
+        status: 0,
+      });
+      res.send(requests);
+    } catch (e) {
+      res.status(500).send(e);
+    }
+  });
+  
+  // router.post("/api/req/:userID", auth, async (req, res) => {
+  //   try {
+  //     const request = await Friends.findById({
+  //       requestedBy: req.params.userID,
+  //       requestedTo: req.user.userID,
+  //     });
+  //     request.status = 1;
+  //     await request.save();
+  //     if (request) {
+  //       const user1 = await user1.findById(req.user._id);
+  //       user1.follower = user1.follower + 1;
+  //       await user1.save();
+  //       const user2 = await user1.findById({ userID: req.params.userID });
+  //       user2.following = user2.following + 1;
+  //       await user2.save();
+  //     }
+  
+  //     const requests = await Friends.find({
+  //       requestedTo: req.user.userID,
+  //       status: 0,
+  //     });
+  //     res.send(requests);
+  //   } catch (e) {
+  //     res.status(500).send(e);
+  //   }
+  // });
+  
+  // router.get("/api/req/:userID/status", auth, async (req, res) => {
+  //   try {
+  //     const request = await Friends.findById({
+  //       requestedTo: req.params.userID,
+  //       requestedBy: req.user.userID,
+  //     });
+  //     if (!request) {
+  //       res.send("Follow");
+  //     } else if (request.status == 0) {
+  //       res.send("Requested");
+  //     } else {
+  //       res.send("Following");
+  //     }
+  //   } catch (e) {
+  //     res.status(500).send(e);
+  //   }
+  // });
+  
+  // router.get("/api/:userID/followers", auth, async (req, res) => {
+  //   try {
+  //     const followers = await Friends.find({
+  //       requestedTo: req.params.userID,
+  //       status: 1,
+  //     });
+  //     const userID = await followers.map((follower) => {
+  //       return follower.requestedBy;
+  //     });
+  //     res.send(userID);
+  //   } catch (e) {
+  //     res.status(500).send(e);
+  //   }
+  // });
+  
+  // router.get("/api/:userID/following", auth, async (req, res) => {
+  //   try {
+  //     const following = await Friends.find({
+  //       requestedBy: req.params.userID,
+  //       status: 1,
+  //     });
+  //     const userID = await following.map((follow) => {
+  //       return follow.requestedTo;
+  //     });
+  //     res.send(userID);
+  //   } catch (e) {
+  //     res.status(500).send(e);
+  //   }
+  // });
+  module.exports = router
