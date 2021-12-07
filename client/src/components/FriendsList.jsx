da85640c9c2d7cbb4227700e1b8d0228e88a5780
n-rdy <giuseppegelardi@icloud.com>
2021-12-06 22:45:38 -0600

    add friends to cards

--- client/src/components/FriendsList.jsx
+++ client/src/components/FriendsList.jsx
@@ -1,29 +1,72 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import axios from 'axios'
+import { Avatar, Card, CardContent, CardHeader, Container, Grid, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
 
 import UserContext from '../context/UserContext'
 
+const useStyles = makeStyles({
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  text: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: 'block',
+  },
+})
+
 export default function FriendsList() {
   const { user } = useContext(UserContext)
-  let friendsArray = []
+  const [friendsList, setFriendsList] = useState([])
+  const classes = useStyles()
+  // let friendsArray = []
 
   const getFriendsList = () => {
     user.friendsList?.map(async friends => {
       let response = await axios.get(`http://localhost:8000/api/users/${friends}`)
-      if (response.data) friendsArray.push(response.data)
+      setFriendsList(friendsList => [...friendsList, response.data])
     })
-    return friendsArray
+    return friendsList
   }
 
   useEffect(() => {
     getFriendsList()
-  })
+  }, [user])
 
   return (
     <div>
       <h1>display friends list</h1>
       <button onClick={() => console.log(user)}>log user</button>
-      <button onClick={() => console.log(friendsArray)}>log friends list</button>
+      <button onClick={() => console.log(friendsList)}>friends list</button>
+      
+      <Container className={classes.field} style={{ display: 'flex', marginTop: '75px'}}>
+        <Grid container spacing={3}>
+          {friendsList.map(friends => 
+          <Grid item key={friends.name} sx={12} md={12} lg={12}>
+
+          
+          <Card elevation={5} key={friends.name}>
+            <CardHeader 
+              avatar={
+                <Avatar>
+                  {friends.name[0]}
+                </Avatar>
+              }
+            />
+            <CardContent>
+              <Typography variant='body2' color='textSecondary'>
+                {friends.name}
+              </Typography>
+            </CardContent>
+
+          </Card>
+          </Grid>
+          )}
+        </Grid>
+
+      </Container>
     </div>
   )
 }
