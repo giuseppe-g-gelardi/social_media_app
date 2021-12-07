c178039024978aacc360dbe62c6afccb55420542
Sebastian Maxwell <sebastianrmaxwell@gmail.com>
2021-12-07 18:31:43 -0500

    Rework private messages. Testing in postman

--- models/privatemessages.js
+++ models/privatemessages.js
@@ -1,97 +1,10 @@
-const mongoose = require("mongoose");
+import mongoose from 'mongoose';
 
-const MessageSchema = new mongoose.Schema(
-  {
-    from: {
-      type: String,
-      required: true,
-    },
-    to: {
-      type: String,
-      required: true,
-    },
-    body: {
-      type: String,
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
+const MessageSchema = mongoose.Schema( {
+    message: String,
+    name: String,
+    timestamp: String,
+    received: Boolean
+})
 
-const Message = mongoose.model("Message", MessageSchema);
-
-module.exports = Message;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
-// const dateAndTime = require("date-and-time");
-// const date = new Date();
-
-// const messageShema = Schema({
-//   user: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: "users",
-//   },
-//   text: {
-//     type: String,
-//     required: true,
-//   },
-//   date: {
-//     type: String,
-//     default: dateAndTime.format(date, "hh:mm:ss A, MM-DD-YYYY"),
-//   },
-// });
-
-// module.exports = mongoose.model("message", messageShema);
-
-
-////
-
-
-// const Joi = require("joi");
-// const mongoose = require("mongoose");
-
-
-// const messageSchema = new mongoose.Schema({
-//     userID:{ type:String, required:true },
-//     text: { type: String, required: true },
-// });
-
-// function validateMessage(message) {
-//     const schema = Joi.object({
-//       userID: Joi.string().min(1).max(255).required(),
-//       text: Joi.string().min(1).max(255).required(),
-//       timestamp: { type: Date, default: Date.now()}
-      
-//     });
-//     return schema.validate(message);
-//   }
-
-// const Message = mongoose.model("Message", messageSchema);
-
-// module.exports.Message = Message;
-// module.exports.validateMessage = validateMessage;
+export default mongoose.model('message',MessageSchema)
--- routes/privatemessages.js
+++ routes/privatemessages.js
@@ -1,119 +1,34 @@
-const User = require("../models/user");
+
 const express = require("express");
-const auth = require("../middlewares/auth");
 const router = new express.Router();
-const Friend = require("../models/friends");
-const Message = require("../models/message");
 
 
-Router.get("/api/message/:userName", auth, async (req, res) => {
-  try {
-    const messages = await Message.find({
-      $or: [
-        { to: req.user.userName, by: req.params.userName },
-        { by: req.user.userName, to: req.params.userName },
-      ],
-    }).sort({ _id: -1 });
-    res.send(messages);
-  } catch (e) {
-    res.status(500).send(e);
-  }
-});
+router.get('/messages/sync',(req,res) => {
+    Messages.find((err,data) => {
+        if(err) {
+            res.status(500).send(err)
+        }
+        else {
+            res.status(200).send(data)
+        }
+    })
+})
 
-Router.get("/api/messageList/friends", auth, async (req, res) => {
-  try {
-    const following = await Friend.find({
-      requestedBy: req.user.userName,
-      status: 1,
-    });
+router.post('/messages/new',(req,res) => {
+    const MessageSchema = req.body
 
-    const userNames = await following.map((follow) => {
-      return follow.requestedTo;
-    });
+    Messages.create(MessageSchema,(err,data) => {
+        if(err) {
+            res.status(500).send(err)
+        }
+        else {
+            res.status(201).send(data)
+        }
+    })
+})
 
-    const messages = await Message.find({
-      $or: [
-        { by: req.user.userName, to: { $in: userNames } },
-        { by: { $in: userNames }, to: req.user.userName },
-      ],
-    }).sort({ _id: -1 });
+Router.listen(port,() => console.log(`Listening to localhost:${port}`))
 
-    const users = await messages.map((message) => {
-      return message.to == req.user.userName ? message.by : message.to;
-    });
-
-    let unique = users.filter((v, i, a) => a.indexOf(v) === i);
-
-    const extraUserNames = userNames.filter(
-      (userName) => !unique.includes(userName)
-    );
-    const final = [...unique, ...extraUserNames];
-    res.send(final);
-  } catch (e) {
-    res.status(500).send(e);
-  }
-});
-
-Router.get("/api/messageList/requests", auth, async (req, res) => {
-  try {
-    const following = await Friend.find({
-      requestedBy: req.user.userName,
-      status: 1,
-    });
-
-    const userNames = await following.map((follow) => {
-      return follow.requestedTo;
-    });
-
-    const messages = await Message.find({
-      $or: [
-        { by: req.user.userName, to: { $nin: userNames } },
-        { by: { $nin: userNames }, to: req.user.userName },
-      ],
-    }).sort({ _id: -1 });
-
-    const users = await messages.map((message) => {
-      return message.to == req.user.userName ? message.by : message.to;
-    });
-
-    let unique = users.filter((v, i, a) => a.indexOf(v) === i);
-
-    res.send(unique);
-  } catch (e) {
-    res.status(500).send(e);
-  }
-});
-
-// Router.post("/api/message", [check("text", "Please Provide Message").not().isEmpty()],
-//     async(req, res) => {
-//         const errors = validationResult(req);
-//         if (!errors.isEmpty()) {
-//             return res.status(400).json({ error: errors.array() });
-//     }
-
-//     const { text } = req.body;
-
-//     try {
-//       const user = await User.findOne({ username: req.params.username }).select(
-//         "-password"
-//       );
-
-//       if (!user) {
-//         res.json({ error: [{ msg: "User not found" }] });
-//       }
-
-//       let message = new Message({
-//         user: user.id,
-//         text,
-//       });
-
-//       message = await message.save();
-
-//       res.status(200).json(message);
-//     } catch (err) {
-//       console.error(err.message);
-//       res.status(500).json({ error: [{ msg: "Server Error" }] });
-//     });
 
 
 module.exports = router;
